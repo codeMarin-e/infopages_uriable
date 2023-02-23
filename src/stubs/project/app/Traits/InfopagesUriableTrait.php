df707442f5b1fbd053b7afd3b8e367b1632b092d
Marin <marin@dev.nddesign.no>
2023-02-23 14:50:07 +0200

    fix uriable

--- src/stubs/project/app/Traits/InfopagesUriableTrait.php
+++ src/stubs/project/app/Traits/InfopagesUriableTrait.php
@@ -5,7 +5,7 @@
 
         use \App\Traits\Uriable;
 
-        public function defaultUri($language = null, $site_id = null, $prepareLevel = null) { //just for default
+        public function defaultUri($language = null, $site_id = null, $prepareLevel = null, $additionals = []) { //just for default
             $prepareLevel = $prepareLevel?? config('marinar_infopages_uriable.prepare_levels');
             if($prepareLevel && $this->parent_id) {
                 $this->loadMissing('parent');
@@ -14,7 +14,7 @@
             return 'info/'.$this->id;
         }
 
-        public function prepareSlug($slug, $prepareLevel = null) {
+        public function prepareSlug($slug, $prepareLevel = null, $additionals = []) {
             $prepareLevel = $prepareLevel?? config('marinar_infopages_uriable.prepare_levels');
             if($prepareLevel && $this->parent_id) {
                 $this->loadMissing('parent');
