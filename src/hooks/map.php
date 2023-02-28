fae44e5c35533624bc35dc80c53f8d51f72d583e
Marin <marin@dev.nddesign.no>
2023-02-28 15:16:54 +0200

    fix mapping

--- src/hooks/map.php
+++ src/hooks/map.php
@@ -1,8 +1,4 @@
 <?php
-
-use Marinar\InfopagesUriable\MarinarInfopagesUriable;
-
-$packageDir = MarinarInfopagesUriable::getPackageMainDir();
 return [
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'config','marinar_infopages.php']) => [
         "// @HOOK_INFOPAGES_CONFIGS_ADDONS" => "\t\t\\Marinar\\InfopagesUriable\\MarinarInfopagesUriable::class, \n",
@@ -20,15 +16,15 @@ return [
         "// @HOOK_INFOPAGE_LANG" => "\t'uri_type' => 'Infopage', \n",
     ],
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'resources', 'views', 'admin', 'infopages', 'infopage.blade.php']) => [
-        "{{-- @HOOK_INFOPAGE_AFTER_CONTENT --}}" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGE_AFTER_CONTENT.blade.php']),
+        "{{-- @HOOK_INFOPAGE_AFTER_CONTENT --}}" => implode(DIRECTORY_SEPARATOR, [__DIR__, 'HOOK_INFOPAGE_AFTER_CONTENT.blade.php']),
     ],
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'app', 'Http', 'Controllers', 'Admin', 'InfopageController.php']) => [
-        "// @HOOK_INFOPAGES_STORE_END" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGES_STORE_END.php']),
-        "// @HOOK_INFOPAGES_UPDATE_END" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGES_STORE_END.php']),
+        "// @HOOK_INFOPAGES_STORE_END" => implode(DIRECTORY_SEPARATOR, [__DIR__, 'HOOK_INFOPAGES_STORE_END.php']),
+        "// @HOOK_INFOPAGES_UPDATE_END" => implode(DIRECTORY_SEPARATOR, [__DIR__, 'HOOK_INFOPAGES_STORE_END.php']),
     ],
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'app', 'Http', 'Requests', 'Admin', 'InfopageRequest.php']) => [
-        "// @HOOK_INFOPAGE_REQUEST_RULES" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGE_REQUEST_RULES.php']),
-        "// @HOOK_INFOPAGE_REQUEST_MESSAGES" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGE_REQUEST_MESSAGES.php']),
-        "// @HOOK_INFOPAGE_REQUEST_AFTER_VALIDATED" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGE_REQUEST_AFTER_VALIDATED.php']),
+        "// @HOOK_INFOPAGE_REQUEST_RULES" => implode(DIRECTORY_SEPARATOR, [__DIR__, 'HOOK_INFOPAGE_REQUEST_RULES.php']),
+        "// @HOOK_INFOPAGE_REQUEST_MESSAGES" => implode(DIRECTORY_SEPARATOR, [__DIR__, 'HOOK_INFOPAGE_REQUEST_MESSAGES.php']),
+        "// @HOOK_INFOPAGE_REQUEST_AFTER_VALIDATED" => implode(DIRECTORY_SEPARATOR, [__DIR__, 'HOOK_INFOPAGE_REQUEST_AFTER_VALIDATED.php']),
     ],
 ];
